a112768b7352f0bf2a1cd4406c7dfd5fe7dbd16d
Melnyk Viktoriia <vikavmelnyk@gmail.com>
2023-01-08 22:49:17 +0200

    2 task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+let ingredients_list = document.querySelector('#ingredients');
+
+const createIngredients = ingredientsList => {
+  return ingredientsList.map(ingredient => {
+    const ingredients_item = document.createElement('li');
+    ingredients_item.classList.add('item');
+    ingredients_item.textContent = ingredient;
+
+    return ingredients_item;
+  });
+};
+
+const elements = createIngredients(ingredients);
+ingredients_list.append(...elements);
+
